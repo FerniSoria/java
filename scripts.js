17f2f5fe23df7bcb9ad70e42c4fcf58a9e6e6e46
FerniSoria <87145192+FerniSoria@users.noreply.github.com>
2021-10-03 21:57:08 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 class Paquetes {
 
-    constructor(producName, noches, jornadas, comidas, capacidad, precio) {
-
+    constructor(id, producName, noches, jornadas, comidas, capacidad, precio) {
+        this.id= id;
         this.productName = producName;
         this.noches = noches;
         this.jornadas = jornadas;
@@ -13,26 +13,27 @@ class Paquetes {
 
     }
 
-    getsaleinfo() {
-        // this.productName + " - " + this.noches + " Noches - " + this.jornadas + " Jornadas - " + this.comidas + " Incluye COmida - " + this.capacidad + " Personas Capacidad - " + this.precio + "$" ).toUpperCase ()
-        return (
-            contenedor.innerHTML = `<div class="p-3 m-5 text-center container" >
-              <h3>${this.productName}</h3>
-              <p>Noches - ${this.noches}</p>
-              <p>Jornadas - ${this.jornadas}</p>
-              <p>Comidas - ${this.comidas}</p> 
-              <p>Capacidad - ${this.capacidad}</p>
-              <p>Precio - $ ${this.precio}</p>
+    // getsaleinfo() {
+    //     // this.productName + " - " + this.noches + " Noches - " + this.jornadas + " Jornadas - " + this.comidas + " Incluye COmida - " + this.capacidad + " Personas Capacidad - " + this.precio + "$" ).toUpperCase ()
+    //     return (
+    //         contenedor.innerHTML = `<div class="p-3 m-5 text-center container" >
+    //           <h3>${this.productName}</h3>
+    //           <p>Noches - ${this.noches}</p>
+    //           <p>Jornadas - ${this.jornadas}</p>
+    //           <p>Comidas - ${this.comidas}</p> 
+    //           <p>Capacidad - ${this.capacidad}</p>
+    //           <p>Precio - $ ${this.precio}</p>
 
-            </div>`
-        );
-    }
+    //         </div>`
+    //     );
+    // }
 
 }
-let contenedor = document.querySelector('.contenedor');
-let paquete1 = new Paquetes("Cayasta", "3", "2", "si", 6, 20000);
-let paquete2 = new Paquetes("Esquina", "4", "3", "no", 4, 35000);
-let paquete3 = new Paquetes("Amazonas", "10", "5", "si", 12, 250000);
+// let contenedor = document.querySelector('.contenedor');
+let paquete1 = new Paquetes("1", "Cayasta", "3", "2", "si", 6, 20000);
+let paquete2 = new Paquetes("2", "Esquina", "4", "3", "no", 4, 35000);
+let paquete3 = new Paquetes("3", "Amazonas", "10", "5", "si", 12, 250000);
+
 // let cayasta = 20000
 // let esquina = 35000
 // let amazonas = 250000
@@ -44,9 +45,36 @@ const salidas = [];
 salidas.push(paquete1);
 salidas.push(paquete2);
 salidas.push(paquete3);
-let paquete0 = new Paquetes("Santa Elena", "3", "2", "si", 4, 15000);
+let paquete0 = new Paquetes("0", "Santa Elena", "3", "2", "si", 4, 15000);
 salidas.unshift(paquete0);
 
+
+$( document ).ready(function(){
+            for (let salida of salidas){
+
+                $("#lista").append(`
+                <li class="m-3"> ${salida.productName} <button class="btn btn-info" id="btn${salida.id}"> Mostrar Info </button> </li>
+                `);
+                $(`#btn${salida.id}`).on("click",function(){      
+                    $(".contenedor").empty();
+                    $(".contenedor").append(`<div class="p-3 m-5 text-center container" >
+                   
+                    <h3>${salida.productName}</h3>
+                    <p>Noches - ${salida.noches}</p>
+                    <p>Jornadas - ${salida.jornadas}</p>
+                    <p>Comidas - ${salida.comidas}</p> 
+                    <p>Capacidad - ${salida.capacidad}</p>
+                    <p>Precio - $ ${salida.precio}</p>
+      
+                  </div>
+                    `)
+                   
+
+
+                })
+            }
+
+});
 
 //let ciudades_pesca = ["Cayasta", "Helvecia", "Santa Elena", "La Paz", "San Javier", "Esquina", "Alejandra"];
 //console.log("ciudades para pesca" + ciudades_pesca);
@@ -62,44 +90,44 @@ salidas.unshift(paquete0);
 
 // let service = prompt("").toLowerCase();
 
-btn1 = document.querySelector('.btn1');
-btn2 = document.querySelector('.btn2');
-btn3 = document.querySelector('.btn3');
-btn4 = document.querySelector('.btn4');
+// btn1 = document.querySelector('.btn1');
+// btn2 = document.querySelector('.btn2');
+// btn3 = document.querySelector('.btn3');
+// btn4 = document.querySelector('.btn4');
 
-btn1.addEventListener('click', mostrarPaquete);
-btn2.addEventListener('click', mostrarPaquete1);
-btn3.addEventListener('click', mostrarPaquete2);
-btn4.addEventListener('click', mostrarPaquete3);
+// btn1.addEventListener('click', mostrarPaquete);
+// btn2.addEventListener('click', mostrarPaquete1);
+// btn3.addEventListener('click', mostrarPaquete2);
+// btn4.addEventListener('click', mostrarPaquete3);
 
-function mostrarPaquete (){
+// function mostrarPaquete (){
   
-        contenedor.innerHTML= "";
-        paquete0.getsaleinfo();
+//         contenedor.innerHTML= "";
+//         paquete0.getsaleinfo();
     
 
-}
-function mostrarPaquete1 (){
+// }
+// function mostrarPaquete1 (){
   
-    contenedor.innerHTML= "";
-    paquete1.getsaleinfo();
+//     contenedor.innerHTML= "";
+//     paquete1.getsaleinfo();
 
 
-}
-function mostrarPaquete2 (){
+// }
+// function mostrarPaquete2 (){
   
-    contenedor.innerHTML= "";
-    paquete2.getsaleinfo();
+//     contenedor.innerHTML= "";
+//     paquete2.getsaleinfo();
 
 
-}
-function mostrarPaquete3 (){
+// }
+// function mostrarPaquete3 (){
   
-    contenedor.innerHTML= "";
-    paquete3.getsaleinfo();
+//     contenedor.innerHTML= "";
+//     paquete3.getsaleinfo();
 
 
-}
+// }
 
 
 // switch (service) {
